147028d4c04d38dfa716af636972a14686bcc776
lucaspere <lucasrupestre@hotmail.com>
2023-03-30 18:40:50 -0300

    test: create import dynamic Repository test

---
+++ tests/repositories/index.test.ts
@@ -0,0 +1,21 @@
+import { assert } from 'chai';
+import { useRepository } from '../../src/repositories';
+import RecipientMemoryRepository from '../../src/repositories/MemoryRepository';
+
+suite('Testing Repository Loader', function () {
+    test('Should load with default Repository', async () => {
+        const repo = await useRepository()
+
+        assert.instanceOf(repo, RecipientMemoryRepository)
+
+    });
+    test('Should throw a error when not found repository class', async () => {
+        try {
+            await useRepository("Test Repository")
+        } catch (e: any) {
+            assert.instanceOf(e, Error)
+            return
+        }
+        assert.fail("Should Throw a Error")
+    });
+});
