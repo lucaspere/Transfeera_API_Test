c58c27d1bf563311dfc399ded0becb19e48a9c91
lucaspere <lucasrupestre@hotmail.com>
2023-03-31 00:14:17 -0300

    fix: move the repository import to a variable

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,7 +2,7 @@ import { Repository } from '../types/repository';
 import { Recipient } from '../types/recipient';
 import { InternalServerError } from '../utils/errors';
 import { NODE_ENV } from '../server';
-import { join } from 'path';
+import { resolve } from 'path';
 import { inspect } from 'util';
 
 const EXTENSION = NODE_ENV === 'development' || NODE_ENV == 'test' ? 'ts' : 'js'
@@ -10,7 +10,8 @@ const EXTENSION = NODE_ENV === 'development' || NODE_ENV == 'test' ? 'ts' : 'js'
 let singletonRepo: Repository<Recipient>;
 export const useRepository = async (repositoryType = "MemoryRepository"): Promise<Repository<Recipient>> => {
     try {
-        const Repository = (await import(join(__dirname, `./${repositoryType}.${EXTENSION}`))).default;
+        const path = resolve(__dirname, `./${repositoryType}.${EXTENSION}`)
+        const Repository = (await import(path)).default;
         singletonRepo = new Repository()
         return singletonRepo;
     } catch (error) {
